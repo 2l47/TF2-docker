b165c59a02af3d4414623312f3d9fe3173b7215e
2l47 <2l47@horsefucker.org>
2021-11-03 00:14:48 -0500

    Update variety's rotations.json and remove support for TF2Maps downloads
    Also removed the sv_downloadurl from variety's server.cfg

--- profiles/variety/direct-copy/autorotate.py
+++ profiles/variety/direct-copy/autorotate.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 
 # We determine the rotation to use from the day of the year and the container's instance number (stored by the profile's crontab module as the offset value).
-# If this is the first time running, or rotations.json has been updated, we also download any required maps from all rotations.
-# Finally, we write out the mapcycle.txt for the current rotation.
+# If this is the first time running, or rotations.json has been updated, we regenerate tf/subscribed_file_ids.txt
+# Lastly, we write out the mapcycle.txt for the current rotation.
 
 # Side note: subscribed_file_ids.txt might actually be completely unnecessary for TF2.
 
@@ -57,13 +57,11 @@ def write_mapcycle(rotation):
 			elif map["type"] == "workshop":
 				workshop_id = map["workshop_id"]
 				mapcycle.write(f"workshop/{workshop_id}\n")
-			elif map["type"] == "tf2maps":
-				mapcycle.write(f"{map_name}\n")
 			else:
 				raise SystemExit("Unknown map type")
 
 
-# Check if rotations.json has been updated; if so, update workshop IDs and download maps
+# Check if rotations.json has been updated; if so, update workshop IDs
 previous_hash = None
 # Get the hash of rotations.json as of the previous run (if any)
 if os.path.exists("rotations.json.md5"):
@@ -76,22 +74,10 @@ with open("rotations.json", "rb") as f:
 with open("rotations.json.md5", "w") as md5_f:
 	md5_f.write(current_hash)
 
-# We're going to recreate subscribed_file_ids.txt and download any necessary maps
+# We're going to recreate subscribed_file_ids.txt
 if previous_hash != current_hash:
 	print(f"Previous hash ({previous_hash}) differs from current hash ({current_hash}).")
-	print("Updating subscribed_file_ids.txt and downloading any necessary maps...")
-	# Used for retrieving map filenames
-	import requests
-	session = requests.session()
-	session.headers.update({"User-Agent": f"autorotate.py/{__version__} ({__ghrepo})"})
-	# Regex to match tf2maps download locations
-	import re
-	download_regex = re.compile(r'(?<=href=")downloads/[a-z0-9.]+/download\?version=\d+(?=")')
-	# Used for actually downloading maps
-	import urllib
-	opener = urllib.request.build_opener()
-	opener.addheaders = [("User-Agent", f"autorotate.py/{__version__} ({__ghrepo})")]
-	urllib.request.install_opener(opener)
+	print("Updating subscribed_file_ids.txt...")
 
 	# SRCDS needs to know what workshop files to subscribe to
 	subscribed_file_ids = set()
@@ -106,41 +92,6 @@ if previous_hash != current_hash:
 			if map["type"] == "workshop":
 				workshop_id = map["workshop_id"]
 				subscribed_file_ids.add(workshop_id)
-			elif map["type"] == "tf2maps":
-				# Handle map download from the given URL, e.g. https://tf2maps.net/downloads/clearcut.5682/
-				url = map["url"]
-				# Fetch the map's page
-				print("\t* Fetching map homepage/download locations...")
-				response = session.get(url)
-				# Look for download locations
-				download_locations = download_regex.findall(response.content.decode())
-				if len(download_locations) == 0:
-					print(f"WARNING: Couldn't get any download locations for tf2maps URL ({url}) - this map will be missing from the server! Did the map get updated? Check the URL.")
-					continue
-				elif len(download_locations) != 1:
-					error = f"ERROR: Got {len(download_locations)} download locations (expected 1) for tf2maps URL ({url}):"
-					error += f"\t{download_locations}"
-					error += f"\nPlease create an issue at: {__ghrepo}"
-					raise SystemExit(error)
-				# We need to know the exact map filename; thankfully HTTP HEAD is a thing, so we don't have to use a temporary filename
-				download_url = f"https://tf2maps.net/{download_locations[0]}"
-				print("\t* Getting map filename...")
-				response = requests.head(download_url)
-				cd = response.headers["Content-Disposition"]
-				filenames = re.findall(r'(?<=filename=").+(?=")', cd)
-				if len(filenames) == 0:
-					print(f"WARNING: Couldn't get the destination filename for tf2maps download URL ({download_url}) - this map will be missing from the server!")
-					continue
-				elif len(filenames) != 1:
-					error = f"ERROR: Got {len(filenames)} filenames (expected 1) for tf2maps download URL ({download_url}):"
-					error += f"\t{filenames}"
-					error += f"\nPlease create an issue at: {__ghrepo}"
-					raise SystemExit(error)
-				# Filename
-				map_filename = filenames[0]
-				# Okay Cool This Is The Part Where We Download The Map
-				print("\t* Downloading map...")
-				urllib.request.urlretrieve(download_url, f"tf/maps/{map_filename}")
 
 	# Okay Cool Now Write Out The Up-To-Date "subscribed_file_ids.txt"
 	print(f"Writing {len(subscribed_file_ids)} workshop IDs to subscribed_file_ids.txt")
@@ -151,6 +102,6 @@ else:
 	print("regions.json doesn't appear to have been updated; not rewriting subscribed_file_ids.txt")
 
 
-# We don't care if the monster above ran, we still need to update the mapcycle now
+# Just update the mapcycle now
 # Fortunately this is very easy since I already wrote the function for it above ^:)
 write_mapcycle(tr)
