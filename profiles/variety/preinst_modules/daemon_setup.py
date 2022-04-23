a52bde0f828cad09b34cd44f7f42afbef07614c2
2l47 <2l47@horsefucker.org>
2022-04-22 19:14:58 -0500

    Update varietyd to use the provided timezone for scheduling
    Migrate from the schedule library to the scheduler library for timezone-aware scheduling support

--- profiles/variety/preinst_modules/daemon_setup.py
+++ profiles/variety/preinst_modules/daemon_setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from helpers import header
+import os
 
 
 
@@ -32,6 +33,15 @@ def loader(profile_name, region_name, instance_number, container):
 			offset += 3
 		f.write(f"{offset}\n")
 
+	# varietyd needs to know what timezone to use for the scheduler
+	# Try to get the timezone from the environment variable, otherwise fallback to UTC
+	try:
+		tz_name = os.environ["TIMEZONE"]
+	except KeyError:
+		tz_name = "Etc/UTC"
+	with open(f"{container_data}/scheduler_timezone.txt", "w") as f:
+		f.write(tz_name)
+
 	# Why not use a cron job?
 	# 1 - The docker image doesn't have systemd, so we'd have to spawn the cron daemon in the entry script
 	# 2 - Debian's version of cron doesn't supply the -m flag which would have let us handle job errors with an external script
@@ -49,11 +59,11 @@ def loader(profile_name, region_name, instance_number, container):
 
 	# varietyd requires the following python modules
 	execute(container, "root", "apt install python3-pip -y")
-	execute(container, "steam", "pip3 install python-daemon requests schedule setproctitle")
+	execute(container, "steam", "pip3 install python-daemon pytz requests scheduler setproctitle")
 
 	# The daemon's already been copied into /home/steam/tf-dedicated/
 	# Just edit the entry script to spawn it
 	execute(container, "steam", "sed -i 's_\#!/bin/bash_&\\n\\n./tf-dedicated/varietyd\\n_' entry.sh")
 
 	# Shut it back off
-	container.stop()
+	container.kill()
