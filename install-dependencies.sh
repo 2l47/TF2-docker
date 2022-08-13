c738d23e126f1c69ca90e0f02d66d2bc53ca0243
2l47 <2l47@protonmail.com>
2022-08-12 22:32:46 -0500

    Update install-dependencies.sh

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -6,14 +6,11 @@
 # If the script was called with sudo, we'll want to give the calling user permissions rather than the user we're executing as
 DOCKER_USER=$USER
 if [[ -n $SUDO_USER ]]; then
-	if [[ $SUDO_USER == "root" ]]; then
-		: # Insert condescending comment here
-	fi
-	echo -e "You didn't need to run this with sudo, but whatever. It's handled, so long as your username is in fact $SUDO_USER.\n"
+	echo -e "You didn't need to run this with sudo since it's used in the script, but it's handled, so long as the user that will be managing TF2 docker containers is in fact $SUDO_USER.\n"
 	DOCKER_USER=$SUDO_USER
 else
 	if [[ $EUID -eq 0 ]]; then
-		echo "Looks like you're logged in as root. Everything will probably still work (I haven't tried), but there may be security risks."
+		echo "Looks like you're logged in as root. It might work (I haven't tried), but there may be security risks."
 		echo "You should really consider creating a non-root user account with sudo privileges instead."
 		read -p "Press enter to continue..."
 	fi
@@ -25,6 +22,6 @@ sudo apt install docker-compose python3-pip xkcdpass -y
 sudo pip3 install --target /usr/lib/python3/dist-packages python-a2s
 
 # Give the calling user access to docker
-sudo usermod --append --groups docker $DOCKER_USER
+sudo adduser $DOCKER_USER docker
 
 echo -e "\nTF2-docker dependencies installed (probably). You may need to fully log out and back in before your docker permissions will take effect."
