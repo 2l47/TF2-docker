955d62ae02bce4d2a06ff6152baa8f4a51cbfb26
2l47 <2l47@protonmail.com>
2024-01-24 09:36:06 +0000

    variety: Add update-na-debug.sh

---
+++ vtf-scripts/update-na-debug.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+
+# Timezone for Variety.TF NA servers
+export TIMEZONE="America/Chicago"
+
+time ./setup.py -p variety -r dallas -i 1 -o -e
+time ./setup.py -p variety -r dallas -i 2 -o -e
